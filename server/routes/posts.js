4f03ed20c71705ddd22085c9c1a313ce5dfbc6ee
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-27 12:51:57 +0300

    Added posts route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,14 @@
-import express  from "express";
+import express from 'express';
 
-import { getPosts, createPost } from "../controllers/post.js";
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', getPosts )
-router.post('/', createPost )
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
-
-export default router
+export default router;
