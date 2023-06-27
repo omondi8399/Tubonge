0b7ba2ddb0dcd059956f9e04198cf980a8fb053b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-27 12:53:28 +0300

    Updated Index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { reducers } from './reducers';
 import App from './App';
 import './index.css';
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
