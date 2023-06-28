b115887c76bb06d13be22bb11ffc0e1d995351c2
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 17:58:46 +0300

    Updated PostDetails component

--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -34,6 +34,20 @@ recommendedPosts: {
     },
 },
 loadingPaper: {
-    display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', borderRadius: '15px', height: '39vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+    borderRadius: '15px',
+    height: '39vh',
+},
+commentsOuterContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+},
+commentsInnerContainer: {
+    height: '200px',
+    overflowY: 'auto',
+    marginRight: '30px',
 },
 }));
