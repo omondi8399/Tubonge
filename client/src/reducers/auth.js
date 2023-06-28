84b69b675f8fa4fae04fa1678a47156b5b0c3f08
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 13:20:49 +0300

    Updated auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,18 +1,18 @@
 import * as actionType from '../constants/actionTypes';
 
 const authReducer = (state = { authData: null }, action) => {
-switch (action.type) {
+  switch (action.type) {
     case actionType.AUTH:
-    localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
 
-    return { ...state, authData: action.data, loading: false, errors: null };
+      return { ...state, authData: action.data, loading: false, errors: null };
     case actionType.LOGOUT:
-    localStorage.clear();
+      localStorage.clear();
 
-    return { ...state, authData: null, loading: false, errors: null };
+      return { ...state, authData: null, loading: false, errors: null };
     default:
-    return state;
-}
+      return state;
+  }
 };
 
 export default authReducer;
