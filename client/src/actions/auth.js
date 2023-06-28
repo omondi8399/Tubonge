4e4bde763d9b703d4ca7f4ac58f8dca1537302e7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 13:23:37 +0300

    Updated auth action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,25 +2,25 @@ import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
 export const signin = (formData, router) => async (dispatch) => {
-try {
+  try {
     const { data } = await api.signIn(formData);
 
     dispatch({ type: AUTH, data });
 
     router.push('/');
-} catch (error) {
+  } catch (error) {
     console.log(error);
-}
+  }
 };
 
 export const signup = (formData, router) => async (dispatch) => {
-try {
+  try {
     const { data } = await api.signUp(formData);
 
     dispatch({ type: AUTH, data });
 
     router.push('/');
-} catch (error) {
+  } catch (error) {
     console.log(error);
-}
+  }
 };
