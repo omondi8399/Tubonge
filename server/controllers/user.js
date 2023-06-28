261a64a815b79673d448d36892021da65093aea7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 13:15:49 +0300

    Updated user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,9 +6,9 @@ import UserModal from "../models/user.js";
 const secret = 'test';
 
 export const signin = async (req, res) => {
-const { email, password } = req.body;
+  const { email, password } = req.body;
 
-try {
+  try {
     const oldUser = await UserModal.findOne({ email });
 
     if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
@@ -20,15 +20,15 @@ try {
     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
 
     res.status(200).json({ result: oldUser, token });
-} catch (err) {
+  } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
-}
+  }
 };
 
 export const signup = async (req, res) => {
-const { email, password, firstName, lastName } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
-try {
+  try {
     const oldUser = await UserModal.findOne({ email });
 
     if (oldUser) return res.status(400).json({ message: "User already exists" });
@@ -40,9 +40,9 @@ try {
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
     res.status(201).json({ result, token });
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
     
     console.log(error);
-}
+  }
 };
