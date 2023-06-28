9d3bebb17d04f8d211ba296d2fd5b9c091f2f942
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 18:01:28 +0300

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -15,15 +17,13 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.use("/user", userRouter);
 
 const PORT = process.env.PORT|| 5000;
 
+////
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useFindAndModify', false)
-
-
+mongoose.set('useFindAndModify', false);
