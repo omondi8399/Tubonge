aa2fc6ab4160015d0c1a1a8b65b6c884fbeac984
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 13:22:58 +0300

    Updated Posts component

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -1,52 +1,56 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-media: {
+  media: {
     height: 0,
     paddingTop: '56.25%',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     backgroundBlendMode: 'darken',
-},
-border: {
+  },
+  border: {
     border: 'solid',
-},
-fullHeightCard: {
+  },
+  fullHeightCard: {
     height: '100%',
-},
-card: {
+  },
+  card: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
     borderRadius: '15px',
     height: '100%',
     position: 'relative',
-},
-overlay: {
+  },
+  overlay: {
     position: 'absolute',
     top: '20px',
     left: '20px',
     color: 'white',
-},
-overlay2: {
+  },
+  overlay2: {
     position: 'absolute',
     top: '20px',
     right: '20px',
     color: 'white',
-},
-grid: {
+  },
+  grid: {
     display: 'flex',
-},
-details: {
+  },
+  details: {
     display: 'flex',
     justifyContent: 'space-between',
     margin: '20px',
-},
-title: {
+  },
+  title: {
     padding: '0 16px',
-},
-cardActions: {
+  },
+  cardActions: {
     padding: '0 16px 8px 16px',
     display: 'flex',
     justifyContent: 'space-between',
-},
+  },
+  cardAction: {
+    display: 'block',
+    textAlign: 'initial',
+  },
 });
