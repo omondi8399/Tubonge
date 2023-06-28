656b5e6514ef1d48ba24a0a0183edc3f73fe07a8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-28 13:26:13 +0300

    Updated Form component

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -2,24 +2,24 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-root: {
+  root: {
     '& .MuiTextField-root': {
-    margin: theme.spacing(1),
+      margin: theme.spacing(1),
     },
-},
-paper: {
+  },
+  paper: {
     padding: theme.spacing(2),
-},
-form: {
+  },
+  form: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'center',
-},
-fileInput: {
+  },
+  fileInput: {
     width: '97%',
     margin: '10px 0',
-},
-buttonSubmit: {
+  },
+  buttonSubmit: {
     marginBottom: 10,
-},
+  },
 }));
